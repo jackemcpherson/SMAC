0bfcbc1e820729f2bd243d97f516547435ad5da4
Jack McPherson <jmcpherson@mauriceblackburn.com.au>
2023-05-17 11:16:04 +1000

    Added user input for date range selection to SMACStock class.

--- SMAC.py
+++ SMAC.py
@@ -3,14 +3,15 @@ import numpy as np
 import pandas as pd
 import yfinance as yf
 import seaborn as sns
+from datetime import datetime, timedelta
 
 
 class SMACStock:
-    def __init__(self, ticker, long_lb=120, short_lb=50):
+    def __init__(self, ticker, start_date, end_date, long_lb=120, short_lb=50):
         self.ticker = ticker
         self.long_lb = long_lb
         self.short_lb = short_lb
-        self.input_data = yf.download(self.ticker)
+        self.input_data = yf.download(self.ticker, start=start_date, end=end_date)
         self.signal_df = self.calculate_signals()
 
     def calculate_signals(self):
@@ -64,5 +65,31 @@ if __name__ == "__main__":
     long_lb = int(input("Enter the long lookback period: "))
     short_lb = int(input("Enter the short lookback period: "))
 
-    smac = SMACStock(ticker, long_lb=long_lb, short_lb=short_lb)
+    print("Choose a time range for the stock data:")
+    print("1. 1 Month")
+    print("2. 3 Months")
+    print("3. 6 Months")
+    print("4. 1 Year")
+    print("5. 3 Years")
+    print("6. 5 Years")
+
+    time_range = int(input("Enter the number corresponding to your choice: "))
+
+    end_date = datetime.now()
+    if time_range == 1:
+        start_date = end_date - timedelta(days=30)
+    elif time_range == 2:
+        start_date = end_date - timedelta(days=90)
+    elif time_range == 3:
+        start_date = end_date - timedelta(days=180)
+    elif time_range == 4:
+        start_date = end_date - timedelta(days=365)
+    elif time_range == 5:
+        start_date = end_date - timedelta(days=3 * 365)
+    elif time_range == 6:
+        start_date = end_date - timedelta(days=5 * 365)
+    else:
+        print("Invalid choice. Defaulting to 1 year data.")
+        start_date = end_date - timedelta(days=365)
+    smac = SMACStock(ticker, start_date, end_date, long_lb=long_lb, short_lb=short_lb)
     smac.plot(show_actual=True)
